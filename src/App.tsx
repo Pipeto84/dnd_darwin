b4d11f0f1793d6348d3117194be506a1b6eb6f2e
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-11 18:42:43 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import styles from './App.module.css'
+
 function App() {
   return (
-    <div>
-
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Harvesting Process</h2>
+        <p className={styles.infoTitle}>En este formulario usted debe configurar los requerimientos básicos por el FDA para ser declarados por producto.</p>
+      </header>
     </div>
   );
 }
