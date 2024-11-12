2545ad9f231353df10618fe42847f54e5ed40d30
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-11 20:34:21 -0500

    products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import type { CascaderProps } from "antd";
+import { Cascader } from "antd";
+
+interface Products {
+  value: string;
+  label: string;
+}
 
 function App() {
+  const products: Products[] = [
+    {
+      value: "Product 1",
+      label: "Product 1",
+    },
+    {
+      value: "Product 2",
+      label: "Product 2",
+    },
+    {
+      value: "Product 3",
+      label: "Product 3",
+    },
+  ];
+
+  const onChangeProducts: CascaderProps<Products>["onChange"] = (value) => {
+    console.log(value);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h2 className={styles.title}>Harvesting Process</h2>
-        <p className={styles.infoTitle}>En este formulario usted debe configurar los requerimientos básicos por el FDA para ser declarados por producto.</p>
+        <h1 className={styles.titleHeader}>Harvesting Process</h1>
+        <p className={styles.infoHeader}>
+          En este formulario usted debe configurar los requerimientos básicos
+          por el FDA para ser declarados por producto.
+        </p>
       </header>
+      <div className={styles.form}>
+        <div className={styles.products}>
+          <h3>Products</h3>
+          <Cascader
+            className={styles.productsList}
+            options={products}
+            onChange={onChangeProducts}
+          />
+        </div>
+        <hr className={styles.line}/>
+      </div>
     </div>
   );
 }
