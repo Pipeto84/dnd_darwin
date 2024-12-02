8764e21bc3754cdcb3b383605141fa51e95c11eb
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-12-02 10:43:52 -0500

    collapse flecha final

--- src/components/Drags.tsx
+++ src/components/Drags.tsx
@@ -1,17 +1,79 @@
-import styles from '../Styles/Drags.module.css'
+import styles from "../Styles/Drags.module.css";
+import { Collapse } from "antd";
+import type { CollapseProps } from "antd";
 
 export const Drags = () => {
+  const generalElements: CollapseProps["items"] = [
+    {
+      key: "1",
+      label: "Add General Element",
+      children: (
+        <div className={styles.generalElements}>
+          <div className={styles.generalElement}>Element 1</div>
+          <div className={styles.generalElement}>Element 2</div>
+          <div className={styles.generalElement}>Element 3</div>
+          <div className={styles.generalElement}>Element 4</div>
+          <div className={styles.generalElement}>Element 5</div>
+          <div className={styles.generalElement}>Element 6</div>
+        </div>
+      ),
+    },
+  ];
+  const formField: CollapseProps["items"] = [
+    {
+      key: "2",
+      label: "Add Form Field",
+      children: (
+        <div className={styles.generalElements}>
+          <div className={styles.generalElement}>Element 1</div>
+          <div className={styles.generalElement}>Element 2</div>
+          <div className={styles.generalElement}>Element 3</div>
+          <div className={styles.generalElement}>Element 4</div>
+          <div className={styles.generalElement}>Element 5</div>
+          <div className={styles.generalElement}>Element 6</div>
+        </div>
+      ),
+    },
+  ];
+  const layoutFields: CollapseProps["items"] = [
+    {
+      key: "3",
+      label: "Layout Elements",
+      children: (
+        <div className={styles.generalElements}>
+          <div className={styles.generalElement}>Element 1</div>
+          <div className={styles.generalElement}>Element 2</div>
+          <div className={styles.generalElement}>Element 3</div>
+          <div className={styles.generalElement}>Element 4</div>
+          <div className={styles.generalElement}>Element 5</div>
+          <div className={styles.generalElement}>Element 6</div>
+        </div>
+      ),
+    },
+  ];
+  const themes: CollapseProps["items"] = [
+    {
+      key: "4",
+      label: "Add Form Field",
+      children: (
+        <div className={styles.generalElements}>
+          <div className={styles.generalElement}>Element 1</div>
+          <div className={styles.generalElement}>Element 2</div>
+          <div className={styles.generalElement}>Element 3</div>
+          <div className={styles.generalElement}>Element 4</div>
+          <div className={styles.generalElement}>Element 5</div>
+          <div className={styles.generalElement}>Element 6</div>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div className={styles.containerGeneralElements}>
-      <label className={styles.titleGeneralElements}>Add General Element</label>
-      <div className={styles.generalElements}>
-        <div className={styles.generalElement}>Element 1</div>
-        <div className={styles.generalElement}>Element 2</div>
-        <div className={styles.generalElement}>Element 3</div>
-        <div className={styles.generalElement}>Element 4</div>
-        <div className={styles.generalElement}>Element 5</div>
-        <div className={styles.generalElement}>Element 6</div>
-      </div>
+      <Collapse defaultActiveKey={["1"]} ghost items={generalElements} expandIconPosition="end"/>
+      <Collapse defaultActiveKey={["2"]} ghost items={formField} expandIconPosition="end"/>
+      <Collapse ghost items={layoutFields} expandIconPosition="end"/>
+      <Collapse ghost items={themes} expandIconPosition="end"/>
     </div>
-  )
-}
+  );
+};
