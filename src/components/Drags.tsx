d8022c8d787d74309e6d1c2965d07a68c54f5528
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-12-02 08:36:20 -0500

    sin collapse

--- src/components/Drags.tsx
+++ src/components/Drags.tsx
@@ -2,8 +2,16 @@ import styles from '../Styles/Drags.module.css'
 
 export const Drags = () => {
   return (
-    <div className={styles.elements}>
-      
+    <div className={styles.containerGeneralElements}>
+      <label className={styles.titleGeneralElements}>Add General Element</label>
+      <div className={styles.generalElements}>
+        <div className={styles.generalElement}>Element 1</div>
+        <div className={styles.generalElement}>Element 2</div>
+        <div className={styles.generalElement}>Element 3</div>
+        <div className={styles.generalElement}>Element 4</div>
+        <div className={styles.generalElement}>Element 5</div>
+        <div className={styles.generalElement}>Element 6</div>
+      </div>
     </div>
   )
 }
