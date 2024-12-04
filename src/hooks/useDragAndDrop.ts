0268eb498a1c16c2c99c68e520c49198f656a3a1
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-12-04 15:55:01 -0500

    Drag and Drop 2

--- src/hooks/useDragAndDrop.ts
+++ src/hooks/useDragAndDrop.ts
@@ -7,16 +7,10 @@ export const useDragAndDrop = (initialState: DataCard[][]) => {
   const handleUpdateList = (id: string, group: string) => {
     listItems.map((collapse) => {
       let card = collapse.find((item) => item.id === id);
-      console.log(card?.group, id, group)
 
       if (card && card.status !== "form") {
         card.status = "form";
-        setListItems((prev) =>
-          prev.map((list) => 
-            card!.group !== group ? 
-            [card! , ...list.filter((item) => item.id !== id)] : [...list]
-          )
-        );
+        setListItems((prev) => prev.map((list) => [...list]));
       }
     });
   };
