f94ecca050732bf0d8d09b8d631f11b70f29dcba
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-12-05 11:42:56 -0500

    hook with dragging

--- src/hooks/useDragAndDrop.ts
+++ src/hooks/useDragAndDrop.ts
@@ -5,6 +5,10 @@ export const useDragAndDrop = (initialState: DataCard[][]) => {
 
   const [listItems, setListItems] = useState(initialState);
 
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragging = (dragging: boolean) => setIsDragging(dragging);
+
   const handleUpdateList = (id: string) => {
     listItems.map((collapse) => {
       let card = collapse.find((item) => item.id === id);
@@ -19,5 +23,7 @@ export const useDragAndDrop = (initialState: DataCard[][]) => {
   return {
     listItems,
     handleUpdateList,
+    isDragging,
+    handleDragging
   };
 };
